34add7256625c9b3e439826eb887428d716b3ed7
queen <youlka25@yandex.ru>
2024-02-18 14:58:42 +0300

    init project

---
+++ migrations/m240218_115208_create_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+class m240218_115208_create_user extends Migration
+{
+    public function safeUp()
+    {
+
+        $this->createTable('user', [
+            'id'                   => $this->primaryKey(),
+            'username'             => $this->string()->notNull()->unique(),
+            'auth_key'             => $this->string(32)->notNull(),
+            'password_hash'        => $this->string()->notNull(),
+            'password_reset_token' => $this->string()->unique(),
+            'email'                => $this->string()->notNull()->unique(),
+            'status'               => $this->smallInteger()->notNull()->defaultValue(10),
+            'created_at'           => $this->integer()->notNull(),
+            'updated_at'           => $this->integer()->notNull(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('user');
+    }
+}
