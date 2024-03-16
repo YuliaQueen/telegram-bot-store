c994de006ca076eabba1b89a2cc3d8ff2a142823
queen <youlka25@yandex.ru>
2024-03-16 15:36:15 +0300

    Register Asset Bundles and add css

--- modules/store/views/layouts/main.php
+++ modules/store/views/layouts/main.php
@@ -3,22 +3,37 @@
 use app\assets\AppAsset;
 use app\assets\TelegramWebAppAsset;
 use yii\bootstrap5\Html;
+use yii\web\View;
 
 /** @var yii\web\View $this */
 /** @var string       $content */
 
 AppAsset::register($this);
-TelegramWebAppAsset::register($this);
+$this->registerAssetBundle(TelegramWebAppAsset::class, View::POS_HEAD);
+$this->registerJsFile('@web/js/store/main.js', ['depends' => [TelegramWebAppAsset::class], 'position' => View::POS_HEAD]);
 ?>
-<?php $this->beginPage() ?>
+<?php $this->beginPage(); ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
 <head>
-    <title><?= Html::encode($this->title) ?></title>
-    <?php $this->head() ?>
+    <title><?= Html::encode($this->title); ?></title>
+    <?php $this->head(); ?>
+    <style>
+        body {
+            background-color: var(--tg-theme-bg-color);
+            color: var(--tg-theme-text-color);
+        }
+        button {
+            background-color: var(--tg-theme-button-color);
+            color: var(--tg-theme-button-text-color);
+            border: 0;
+            padding: 5px 10px;
+            border-radius: 5px;
+        }
+    </style>
 </head>
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+<?php $this->beginBody(); ?>
 
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
@@ -26,7 +41,7 @@ TelegramWebAppAsset::register($this);
     </div>
 </main>
 
-<?php $this->endBody() ?>
+<?php $this->endBody(); ?>
 </body>
 </html>
-<?php $this->endPage() ?>
+<?php $this->endPage(); ?>
